a4ed0877f89a988c642979da3572769478cea0bd
azmath2762 <70561336+azmath2762@users.noreply.github.com>
2020-09-14 11:45:10 +0530

    Add files via upload
    ASSIGNMENT 6

---
+++ assignment 6.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+class bank():
+    def __init__(self, name, balance):
+        self.name=name
+        self.balance = balance
+        
+    def getDetails(self):
+        print("NAME =",self.name)
+        print("ACCOUNT BALANCE =", self.balance)
+    
+    
+   
+
+
+# In[6]:
+
+
+ram = bank("azmath", 500)
+
+
+# In[7]:
+
+
+ram.getDetails()
+
+
+# In[10]:
+
+
+AMOUNT = float(input("ENTER AMOUMT"))
+
+
+def withdraw():
+    if( withdraw >= self.balance):
+        print("enetred amount is :" )
+    else:
+        print("entered amount is exceeding balance")
+        
+        
+
+
+# In[11]:
+
+
+AMOUNT = float(input("ENTER AMOUMT"))
+
+def deposit():
+    
+    if num in range(500,10000):
+        temp = num;
+        print("entered amount is :" %temp)
+    else:
+        print("entered amount is exceded")
+
+
+# In[22]:
+
+
+class cone():
+    def __init__(self, radius, height):
+        self.radius = radius
+        self.height = height
+    
+    def getDetails(self):
+        print("radius =", self.radius)
+        print("height =",self.height)
+
+
+# In[23]:
+
+
+dia = cone(30,50)
+
+
+# In[24]:
+
+
+dia.getDetails()
+
+
+# In[31]:
+
+
+import cmath
+
+r = int(input("enter radius :"))
+h = int(input("enter height :"))
+
+volume = 22/7*r*r*(h/3)
+base = 22/7*r*r
+side_area = 22/7*r*r*(r*r+h*h)*(r*r+h*h)
+
+print (volume)
+
+print (base)
+
+print(side_area)
+
+
+# In[ ]:
+
+
+import cmath
+
+
+
